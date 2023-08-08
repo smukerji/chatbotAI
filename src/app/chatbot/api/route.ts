529fe585277c0d0a098f1b3e0fc13ee7dc17201b
jayeshcreole <jayeshp.creolestudios@gmail.com>
2023-08-08 17:34:33 +0530

    Created chat bot with delete and listing functionality

---
+++ src/app/chatbot/api/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTHORIZATION}`,
+    },
+  };
+
+  try {
+    /// fetch the chatbot data
+    const response = await fetch(
+      "https://www.chatbase.co/api/v1/get-chatbots",
+      options
+    );
+
+    if (!response.ok) {
+      throw new Error("Error while retriving chatbots");
+    }
+
+    const data = await response.json();
+    console.log("Called", data);
+
+    // console.log("Data...........", data);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log("Error in chatbot route", error);
+  }
+}
+
+export const fetchCache = "force-no-store";
---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { Button } from "antd";
+import React, { useEffect, useState } from "react";
+import "./chatbot.css";
+import { MessageOutlined } from "@ant-design/icons";
+import axios from "axios";
+import Link from "next/link";
+
+function ChatBot() {
+  /// chatbots details state
+  const [chatbotData, setChatbotData] = useState([]);
+  /// retrive the chatbots details
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        /// get chatbot details
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_WEBSITE_URL}chatbot/api`
+        );
+        setChatbotData(response.data.chatbots);
+      } catch (error) {
+        console.error("Error fetching chatbot data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  /// view chatbot
+  function openChatbot(id: any) {
+    /// send the user to dashboard page
+    window.location.href = `${
+      process.env.NEXT_PUBLIC_WEBSITE_URL
+    }chatbot/dashboard?${encodeURIComponent("chatbot")}=${encodeURIComponent(
+      JSON.stringify(
+        chatbotData.filter((data: any) => {
+          return data.id == id;
+        })[0]
+      )
+    )}`;
+  }
+
+  return (
+    <center>
+      <div className="chatbot-container">
+        <div className="chatbot-container-title">
+          <span className="title-text">My Chatbots</span>
+          <Link href={`${process.env.NEXT_PUBLIC_WEBSITE_URL}`}>
+            <Button style={{ width: "150px" }} type="primary">
+              New Chatbot
+            </Button>
+          </Link>
+        </div>
+        <div className="chatbots">
+          {chatbotData.map((data: any) => {
+            return (
+              <div
+                className="chatbot"
+                key={data.id}
+                onClick={() => openChatbot(data.id)}
+              >
+                <div className="icon">
+                  <MessageOutlined />
+                </div>
+                <div className="name">{data.name}</div>
+              </div>
+            );
+          })}
+        </div>
+        {chatbotData.length == 0 && (
+          <p style={{ color: "red" }}>
+            No chatbots available please create one
+          </p>
+        )}
+      </div>
+    </center>
+  );
+}
+
+export default ChatBot;
