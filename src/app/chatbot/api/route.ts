2ab425a16147ceea45b84a8945bddf23e79dc9b9
jayeshcreole <jayeshp.creolestudios@gmail.com>
2023-08-23 10:51:27 +0530

    checking if mongodb creates the function error

--- src/app/chatbot/api/route.ts
+++ src/app/chatbot/api/route.ts
@@ -40,6 +40,7 @@ export async function POST(request: any) {
 
 // Fetch custom chatbots from the database
 async function fetchCustomBots(userId: string) {
+  return [];
   const db = await connectDatabase();
   const collection = db.collection("user-details");
   const customBots = await collection.find({ userId: userId }).toArray();
