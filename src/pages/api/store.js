de4f4e45134a96a65a3c1ae90796b2ca49f72f8b
jayeshcreole <jayeshp.creolestudios@gmail.com>
2024-01-31 09:31:08 +0530

    live bug fix 6

--- src/pages/api/store.js
+++ src/pages/api/store.js
@@ -15,7 +15,7 @@ import {
 import { ObjectId } from "mongodb";
 const formidable = require("formidable");
 
-export const maxDuration = 300;
+// export const maxDuration = 300;
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
