53a5765244f4897b7aaddbcc4f23a7d7884442a2
jayesht <jayeshp.creolestudios@gmail.com>
2024-02-09 12:47:51 +0530

    popup modal added for hubspot integration

--- src/app/(secure)/chatbot/dashboard/_components/Integration/Integration.tsx
+++ src/app/(secure)/chatbot/dashboard/_components/Integration/Integration.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./integration.scss";
 import Image from "next/image";
 import whatsAppIcon from "../../../../../../../public/svgs/whatsapp-icon.svg";
 import telegramIcon from "../../../../../../../public/svgs/telegram-icon.svg";
+import HubspotModal from "../Modal/HubspotModal";
+import hubspotIcon from '../../../../../../../public/create-chatbot-svgs/Hubspot.svg';
 
-function Integration() {
+function Integration({chatbotId}:any) {
+  const [isHubspotModalOpen, setIsHubspotModalOpen] = useState<boolean>(false); // State to control visibility of HubspotModal
+
   return (
     <div className="integration-container">
       {/*------------------------------------------Whatsapp-integration----------------------------------------------*/}
@@ -24,6 +28,22 @@ function Integration() {
         </div>
         <div className="action">Coming soon</div>
       </div>
+      <div className="integration"  >
+        <div className="name">
+          <Image src={hubspotIcon} alt="telegram-icon" />
+          <span>Add to Hubspot</span>
+        </div>
+        <button className="action-btn" onClick={() => setIsHubspotModalOpen(true)}>Connect</button>
+      </div>
+
+        {/* Render HubspotModal if isHubspotModalOpen is true */}
+        {isHubspotModalOpen && (
+        <HubspotModal
+        chatbotId={chatbotId}
+          isModalOpen={isHubspotModalOpen}
+          setIsModalOpen={setIsHubspotModalOpen}
+        />
+      )}
     </div>
   );
 }
