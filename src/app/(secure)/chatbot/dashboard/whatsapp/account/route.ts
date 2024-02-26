0e46181421dfd332cb430227bdf1685de76d62da
jayeshr <jayeshp.creolestudios@gmail.com>
2024-02-26 15:55:46 +0530

    toggle disable-enable whatsapp api

--- src/app/(secure)/chatbot/dashboard/whatsapp/account/route.ts
+++ src/app/(secure)/chatbot/dashboard/whatsapp/account/route.ts
@@ -6,6 +6,7 @@ import { ObjectId } from "mongodb";
 module.exports = apiHandler({
     DELETE: deleteWhatsappData,
     GET: getWhatsappData,
+    PUT: enableDisableToggle
   });
 
 async function deleteWhatsappData(req: NextRequest) {
@@ -40,4 +41,29 @@ async function getWhatsappData(req: NextRequest) {
     }
     
     return { message: "error" };
+}
+
+//put request for enable/disable
+async function enableDisableToggle(req: NextRequest) {
+  
+  try {
+    let whatsAppDetailsId:any = req.nextUrl.searchParams.get("whatsappDetailsId");
+    const db = (await connectDatabase())?.db();
+    const collection = db?.collection('whatsappbot_details');
+    const foundResult = await collection?.findOne({ _id: new ObjectId(whatsAppDetailsId) });
+
+    if (foundResult) {
+      let result = await collection?.updateOne({ _id: new ObjectId(whatsAppDetailsId) }, { $set: { isEnabled: !foundResult.isEnabled } });
+
+      if(result?.modifiedCount > 0){
+        return { message: "success" };
+      }
+      return { message: "error" };  
+    }
+    
+    return { message: "error" };
+  } catch (error:any) {
+    console.error(error);
+    return { message: "error", error: error.message };
+  }
 }
