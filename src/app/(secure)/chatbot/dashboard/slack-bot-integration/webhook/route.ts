39c3d3fc2186e049fbe83958b03eb2609deef079
jayeshr <jayeshp.creolestudios@gmail.com>
2024-03-05 18:56:22 +0530

    getting message from slack with event api

--- src/app/(secure)/chatbot/dashboard/slack-bot-integration/webhook/route.ts
+++ src/app/(secure)/chatbot/dashboard/slack-bot-integration/webhook/route.ts
@@ -1,16 +1,58 @@
 import { connectDatabase } from "@/db";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
+// import { }
+import { createEventAdapter } from '@slack/events-api';
+const slackSigngingSecret = '73975faec29aaa00bd9810d50b64423d';
+const slackEvents = createEventAdapter(slackSigngingSecret);
+
 
 export async function POST(req: NextRequest) {
-   
+
+  try {
+
     //read the incomming parameter from webhook
-    let resData:any = await req.json();
-  
+    let resData: any = await req.json();
+    // console.log('resData', resData);
+    // slackEventsQueue.add('incommingMessage',{data:resData});
+    writeInDataBase(resData);
+    return new NextResponse('', { status: 200 });
+    if (resData.challenge) {
+      // return new Response(JSON.stringify({ challenge: resData.challenge }), { status: 200 });
+      new NextResponse(JSON.stringify({ challenge: resData.challenge }), { status: 200 });
+
+      let prom = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('done');
+        }, 1000);
+      }
+      );
+
+      let result = await prom;
+      console.log('result', result);
+
+    }
+    else {
+     if(resData.event.type === 'app_mention'){
+      //  console.log('app_mention',resData.event);
+      //  console.log('app_mention',resData.event.text);
+
+      //  slackEventsQueue.add({ event: resData });
+      new NextResponse('', { status: 200 });
+
+      console.log("printing the response from slack bot\t",resData.event.text);
+
+      //  return new Response('', { status: 200 });
+
+     }
+
+    }
+
     console.log('collection', resData);
     // return {
     //     challenge: resData.challenge
     // }
-    return new Response(JSON.stringify({ challenge: resData.challenge }), { status: 200 });
+    // return new Response(JSON.stringify({ challenge: resData.challenge }), { status: 200 });
     const db = (await connectDatabase())?.db();
     const collection = db?.collection('slack-bot_details');
     //find the token in database
@@ -20,17 +62,54 @@ export async function POST(req: NextRequest) {
     //   hubToken === tokenDetails?.webhook_verification_token
     //   // hubToken === process.env.WHATSAPPCALLBACKTOKEN
     // ) {
-  
+
     //   // find whome the hubToken belongs to and update the isTokenVerified to true
     //   const tokenDetails = await collection?.findOne({ webhook_verification_token: hubToken });
     //   if (tokenDetails) {
     //     await collection?.updateOne({ webhook_verification_token: hubToken }, { $set: { isTokenVerified: true } });
     //   }
-  
+
     //   console.log('verified successfully');
     //   return new Response(hubChallenge);
     // }
-  
-    return new Response("Invalid Credentials", { status: 400 });
+
+    // return new Response("Invalid Credentials", { status: 400 });
   }
+  catch (error: any) {
+    console.log('error', error);
+  }
+
+}
+
+
+async function writeInDataBase(data:any){
+  try{
+    let prom = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: data
+        });
+      }, 5000);
+    });
+
+    let result = await prom;
+    console.log('result', result);
+  }
+  catch(error:any){
+    console.log('error',error);
+  }
+  // slackEventsQueue.add('incommingMessage',{data:'test'});
+}
+
+
+// slackEventsQueue.process(async(job:any,done:any)=>{
+
+//   const event = job;
+
+//   console.log('event',event);
+
+//   done();
+// })
+
+
   
