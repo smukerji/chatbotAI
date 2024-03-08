e21621d499589e404561a6bd941ec456bccc191a
jayeshr <jayeshp.creolestudios@gmail.com>
2024-03-08 17:55:25 +0530

    code improved

--- src/app/(secure)/chatbot/dashboard/slack-bot-integration/webhook/route.ts
+++ src/app/(secure)/chatbot/dashboard/slack-bot-integration/webhook/route.ts
@@ -65,8 +65,8 @@ export async function POST(req: NextRequest) {
 }
 
 
-async function writeInDataBase(data:any){
-  try{
+async function writeInDataBase(data: any) {
+  try {
 
     const appId = data.api_app_id;
     const channelId = data.event.channel;
@@ -77,8 +77,8 @@ async function writeInDataBase(data:any){
 
 
 
-    let appDetails:SlackAppData = await slackCollection.findOne({appId:appId});
-    if(appDetails){
+    let appDetails: SlackAppData = await slackCollection.findOne({ appId: appId });
+    if (appDetails) {
       const authOToken = appDetails.authOToken;
       const chatBotId = appDetails.chatBotId;
       const message = data.event.text.replace(/<@.*?>\s*\n?/, '');
@@ -89,131 +89,140 @@ async function writeInDataBase(data:any){
         logLevel: LogLevel.DEBUG
       });
 
-    //if message is empty then return the message      
+      //if message is empty then return the message      
       if (message.length === 0) {
         await client.chat.postMessage({
-          channel: channelId, //"C06MQEG1SCE",
+          channel: channelId,
           text: "I guess you sent me an empty message.",
           threadId: data.event.ts
         });
       }
       else {
-        const response: any = await fetch(
-          `${process.env.NEXT_PUBLIC_WEBSITE_URL}api/pinecone`,
+
+        //check if user chat count has reached the limit if not then increment the count
+        let userDetailsCollection: any = await db.collection('user-details');
+        let cursor = await userDetailsCollection.aggregate([
           {
-            method: "POST",
-            body: JSON.stringify({
-              userQuery: message,
-              chatbotId: chatBotId,
-              userId: userID,
-            }),
+            $match: { userId: userID }
+          },
+          {
+            $addFields: {
+              limitCross: {
+                $cond: [
+                  {
+                    $gte: [
+                      "$totalMessageCount", "$messageLimit"
+                    ]
+                  }, true, false
+                ]
+              }
+            }
           }
-        );
+        ]);
 
-        /// parse the response and extract the similarity results
-        const respText = await response.text();
-        const similaritySearchResults = JSON.parse(respText).join("\n");
-        // Fetch the response from the OpenAI API
+        let limitCrossResult = await cursor.toArray();
 
-        const responseOpenAI: any = await fetch(
-          "https://api.openai.com/v1/chat/completions",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_KEY}`,
-            },
-            body: JSON.stringify({
-              model: "gpt-3.5-turbo",
-              temperature: 0.5,
-              top_p: 1,
-              messages: [
-                {
-                  role: "system",
-                  content: `Use the following pieces of context to answer the users question.
+        await cursor.close();
+
+        if (limitCrossResult[0].limitCross === false) {
+
+          let isUpdated = false;
+          //increment the message count
+          let updatedResult =  await userDetailsCollection.updateOne({ userId: userID }, { $inc: { totalMessageCount: 1 } });
+          if(updatedResult.modifiedCount > 0){
+            isUpdated = true;
+          }
+          // Fetch the response from the Pinecone API
+          const response: any = await fetch(
+            `${process.env.NEXT_PUBLIC_WEBSITE_URL}api/pinecone`,
+            {
+              method: "POST",
+              body: JSON.stringify({
+                userQuery: message,
+                chatbotId: chatBotId,
+                userId: userID,
+              }),
+            }
+          );
+
+          /// parse the response and extract the similarity results
+          const respText = await response.text();
+          const similaritySearchResults = JSON.parse(respText).join("\n");
+          // Fetch the response from the OpenAI API
+
+          const responseOpenAI: any = await fetch(
+            "https://api.openai.com/v1/chat/completions",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_KEY}`,
+              },
+              body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                temperature: 0.5,
+                top_p: 1,
+                messages: [
+                  {
+                    role: "system",
+                    content: `Use the following pieces of context to answer the users question.
                   If you don't know the answer, just say that you don't know, don't try to make up an answer.
                   ----------------
                   context:
                   ${similaritySearchResults}
                   
                   Answer user query and include images write respect to each line if available`,
-                },
-                // ...messages,
-                {
-                  role: "user",
-                  content: `Answer user query and include images in response if available in the given context 
+                  },
+                  // ...messages,
+                  {
+                    role: "user",
+                    content: `Answer user query and include images in response if available in the given context 
                 
                             query: ${message} `,
-                },
-              ],
-            }),
-          }
-        );
-
-        const openaiBody = JSON.parse(await responseOpenAI.text());
-
-        // after getting response from open ai
-        if (openaiBody.choices[0].message.content) {
-         
-
-          //check if user chat count has reached the limit if not then increment the count
-          let userDetailsCollection:any = await db.collection('user-details');
-          // let userDetails = await userDetailsCollection.findOne({userId:userID});
-          let cursor = await userDetailsCollection.aggregate([
-            {
-              $match: { userId: userID }
-            },
-            {
-              $addFields:{
-                limitCross:{
-                  $cond:[
-                    {$gte:[
-                      "$totalMessageCount","$messageLimit"
-                    ]},true,false
-                  ]
-                }
-              }
+                  },
+                ],
+              }),
             }
-          ]);
+          );
 
-          let limitCrossResult = await cursor.toArray();
+          const openaiBody = JSON.parse(await responseOpenAI.text());
 
-          await cursor.close();
-
-          if(limitCrossResult[0].limitCross === false){
-
+          // after getting response from open ai send the response to slack
+          if (openaiBody.choices[0].message.content) {
             await client.chat.postMessage({
-              channel: channelId, //"C06MQEG1SCE",
+              channel: channelId,
               text: openaiBody.choices[0].message.content,
-              threadId: data.event.ts
+              thread_ts: data.event.ts
             });
 
-            let result = await userDetailsCollection.updateOne({ userId: userID }, { $inc: { totalMessageCount: 1 } });
-            console.log('result',result);
           }
           else {
+
+            //if update result is true and open ai didn't respond then decrement the message count
+            if(isUpdated){
+              await userDetailsCollection.updateOne({ userId: userID }, { $inc: { totalMessageCount: -1 } });
+            }
+
             await client.chat.postMessage({
-              channel: channelId, //"C06MQEG1SCE",
-              text: "You have reached the limit of message count. Please try again later.",
-              threadId: data.event.ts
+              channel: channelId,
+              text: "Lucifer AI is not able to answer for your query. Please try again later.",
+              thread_ts: data.event.ts
             });
           }
-
         }
         else {
           await client.chat.postMessage({
-            channel: channelId, //"C06MQEG1SCE",
-            text: "Lucifer AI is not able to answer for your query. Please try again later.",
-            threadId: data.event.ts
+            channel: channelId,
+            text: "You have reached the limit of message count. Please try again later.",
+            thread_ts: data.event.ts
           });
-
         }
       }
     }
 
   }
-  catch(error:any){
-    console.log('error',error);
+  catch (error: any) {
+    console.log('error', error);
   }
- 
+
 }
