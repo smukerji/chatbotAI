6c4bd34ef29fd94326b344c3ac40a88068e201a1
jayesht <jayeshp.creolestudios@gmail.com>
2024-03-15 12:44:35 +0530

    fixed small url issue while connecting in telegram

--- src/app/(secure)/chatbot/dashboard/_components/Modal/TelegramModal.tsx
+++ src/app/(secure)/chatbot/dashboard/_components/Modal/TelegramModal.tsx
@@ -237,7 +237,7 @@ function TelegramModal({ setIsTelegramModalOpen,isTelegramEdit,setIsTelegramEdit
   const onConnect = async () => {
     if (error?.telegramToken === "") {
       try {
-        let url = `https://api.telegram.org/bot${telegramToken}/setWebhook?url=${process.env.NEXT_PUBLIC_NGROCKURL}/chatbot/dashboard/telegram/webhookTelegram/api?token=${telegramToken}&drop_pending_updates=true`;
+        let url = `https://api.telegram.org/bot${telegramToken}/setWebhook?url=${process.env.NEXT_PUBLIC_WEBSITE_URL}chatbot/dashboard/telegram/webhookTelegram/api?token=${telegramToken}&drop_pending_updates=true`;
         const response = await fetch(url, {
           headers: {
             cache: "no-store",
