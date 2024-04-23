3c72c5f3d88b294aa59d43b6dfa71b32a5a22110
jayeshcreole <jayeshp.creolestudios@gmail.com>
2024-04-23 10:21:43 +0530

    fixed logout issue and initial added API docs via sawgger

---
+++ src/app/api-doc/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { getApiDocs } from "../../../lib/swagger";
+import ReactSwagger from "./react-swagger";
+
+export default async function page() {
+  const spec = await getApiDocs();
+  return (
+    <section className="container">
+      <ReactSwagger spec={spec} />
+    </section>
+  );
+}
