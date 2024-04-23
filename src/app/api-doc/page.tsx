ac3aaa6bf447b88577b56a783d0b1e11ff88e0b8
jayeshcreole <jayeshp.creolestudios@gmail.com>
2024-04-23 11:19:13 +0530

    fixed build issue in swagger

--- src/app/api-doc/page.tsx
+++ src/app/api-doc/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { getApiDocs } from "../../../lib/swagger";
 import ReactSwagger from "./react-swagger";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 export default async function page() {
   const spec = await getApiDocs();
   return (
