3fb120bb06fda7fa620bb05251747e4530c03418
Anuj <anuj@sapahk.ai>
2024-07-29 14:00:48 +0530

    Blogs sitemap change

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,49 +1,49 @@
 import { MetadataRoute } from "next";
-import { getAllBlogs } from "./_helpers/blogContent";
+// import { getAllBlogs } from "./_helpers/blogContent";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const blogs = await getAllBlogs();
-  const blogSiteMap = blogs?.map((blog) => {
-    return {
-      url: `https://torri.ai/blog/${blog.slug}`,
-      changeFrequency: "weekly",
-      lastModified: blog.date,
-      priority: 0.7,
-    };
-  });
+  // const blogs = await getAllBlogs();
+  // const blogSiteMap = blogs?.map((blog) => {
+  //   return {
+  //     url: `https://torri.ai/blog/${blog.slug}`,
+  //     changeFrequency: "weekly",
+  //     lastModified: blog.date,
+  //     priority: 0.7,
+  //   };
+  // });
   return [
     {
       url: "https://torri.ai",
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       lastModified: new Date().toISOString(),
-      priority: 1,
+      // priority: 1,
     },
 
     {
       url: "https://torri.ai/home/pricing",
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       lastModified: new Date().toISOString(),
-      priority: 0.9,
+      // priority: 0.9,
     },
     {
       url: "https://torri.ai/terms",
-      changeFrequency: "yearly",
+      // changeFrequency: "yearly",
       lastModified: new Date().toISOString(),
-      priority: 0.11,
+      // priority: 0.11,
     },
     {
       url: "https://torri.ai/privacy",
 
-      changeFrequency: "yearly",
+      // changeFrequency: "yearly",
       lastModified: new Date().toISOString(),
-      priority: 0.12,
+      // priority: 0.12,
     },
     {
       url: "https://torri.ai/blog",
-      changeFrequency: "weekly",
+      // changeFrequency: "weekly",
       lastModified: new Date().toISOString(),
-      priority: 0.9,
+      // priority: 0.9,
     },
-    ...blogSiteMap,
+    // ...blogSiteMap,
   ];
 }
