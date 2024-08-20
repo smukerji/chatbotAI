988c565b963c62454f21e8009ccf420fb11ae92c
jayeshr <jayeshp.creolestudios@gmail.com>
2024-08-20 14:31:48 +0530

    dashboard's list desing done

--- src/app/(secure)/voicebot/dashboard/page.tsx
+++ src/app/(secure)/voicebot/dashboard/page.tsx
@@ -11,121 +11,133 @@ import React, { useContext, useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import "./dashboard.scss";
 
+
+import leftArrow from "../../../../../public/voiceBot/SVG/arrow-left.svg"
+import callOutgoing from "../../../../../public/voiceBot/SVG/call-outgoing.svg"
+
 import arrowIcon from "../../../../../public/svgs/Feather Icon.svg";
 import Image from "next/image";
 function Dashboard() {
 
-  /// get the bot context
-  // const botContext: any = useContext(CreateBotContext);
-  // const botDetails = botContext?.createBotInfo;
-
-  /// get the bot setting context
-  // const chatbotSettingContext: any = useContext(ChatbotSettingContext);
-  // const chatbotSettings = chatbotSettingContext?.chatbotSettings;
-
-  /// check which action is active
-  // const editChatbot = botDetails?.editChatbot;
 
 
-    return (
-      <div className="edit-chatbot-container">
-        {/*------------------------------------------top-section----------------------------------------------*/}
-        <div className="top">
-          {/*------------------------------------------header----------------------------------------------*/}
-          <div className="sources-header">
-            <div className="title">
-              <Image
-                src={arrowIcon}
-                alt="arrow-icon"
-                style={{ transform: "rotate(180deg)", cursor: "pointer" }}
-                onClick={() => {
-                  // botContext?.resetCreateBotInfo();
-                  // chatbotSettingContext?.resetChatbotSettings();
-                  // window.history.back();
-                }}
-              />
-              {/* <h1>{chatbot?.name}</h1> */}
-              <h1>ChatBot Name</h1>
-              
-            </div>
-
-            <h4>Add your data sources to train your Voice agent</h4>
-            
-                <ul className="options-container">
-                  <li
-                    className={"chatbot"}
-                    value={"chatbot"}
-                    // onClick={() =>
-                    //   botContext?.handleChange("editChatbot")("chatbot")
-                    // }
-                  >
-                    <h3 className="option">chatbot</h3>
-                  </li>
-                  <li
-                    className={"settings"}
-                    value={"settings"}
-                    // onClick={() =>
-                    //   botContext?.handleChange("editChatbot")("settings")
-                    // }
-                  >
-                    <h3 className="option">settings</h3>
-                  </li>
-                  <li
-                    className={"sources"}
-                    value={"sources"}
-                    // onClick={() => {
-                    //   // fetchData();
-                    //   botContext?.handleChange("editChatbot")("sources");
-                    // }}
-                  >
-                    <h3 className="option">data sources</h3>
-                  </li>
-                  <li
-                    className={"integrations"}
-                    value={"integrations"}
-                    // onClick={() =>
-                    //   botContext?.handleChange("editChatbot")("integrations")
-                    // }
-                  >
-                    <h3 className="option">Integrations</h3>
-                  </li>
-                  <li
-                    className={"embedSite" }
-                    value={"embedSite"}
-                    // onClick={() =>
-                    //   botContext?.handleChange("editChatbot")("embedSite")
-                    // }
-                  >
-                    <h3 className="option">Embed on site</h3>
-                  </li>
-
-                  <li
-                    className={"history"}
-                    value={"history"}
-                    // onClick={() =>
-                    //   botContext?.handleChange("editChatbot")("history")
-                    // }
-                  >
-                    <h3 className="option">Conversation History</h3>
-                  </li>
-
-                  <li
-                    className={"leads"}
-                    value={"leads"}
-                    // onClick={() =>
-                    //   botContext?.handleChange("editChatbot")("leads")
-                    // }
-                  >
-                    <h3 className="option">Leads</h3>
-                  </li>
-                </ul>
-                <hr />
+  return (
+      
+    <div className="voice-bot-container">
+      <div className="top">
+        <div className="headers">
+          <div className="header-title">
+            <Image className="image" alt="back_arrow" src={leftArrow}></Image>
+            <h1 className="title">ChatBot Name</h1>
           
           </div>
+          <div className="header-description">
+            <h4 className="description">Add your data sources to train your Voice agent</h4>
+          </div>
         </div>
+        <div className="middle-container">
+          <div className="list-container">
+            <ul className="tool-list">
+              <li>Model</li>
+              <li>Transcriber</li>
+              <li>Voice</li>
+              <li>Tool</li>
+              <li>Advance</li>
+              <li>Analysis</li>
+
+            </ul>
+          </div>
+          <div className="button-container">
+            <Button className="demo-call-button">
+              <div className="button-content">
+                <Image alt="phone-call" src={callOutgoing}></Image>
+                <span className="button-text">Demo Talk</span>
+              </div>
+            </Button>
+            <Button className="publish-button">Publish</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      // <div className="edit-chatbot-container">
+      //   <div className="top">
+      //     <div className="sources-header">
+      //       <div className="title">
+      //         <Image
+      //           src={arrowIcon}
+      //           alt="arrow-icon"
+      //           style={{ transform: "rotate(180deg)", cursor: "pointer" }}
+      //           onClick={() => {
+                  
+      //           }}
+      //         />
+      //         <h1>ChatBot Name</h1>
+              
+      //       </div>
+
+      //       <h4>Add your data sources to train your Voice agent</h4>
+            
+      //           <ul className="options-container">
+      //             <li
+      //               className={"chatbot"}
+      //               value={"chatbot"}
+                    
+      //             >
+      //               <h3 className="option">chatbot</h3>
+      //             </li>
+      //             <li
+      //               className={"settings"}
+      //               value={"settings"}
+                   
+      //             >
+      //               <h3 className="option">settings</h3>
+      //             </li>
+      //             <li
+      //               className={"sources"}
+      //               value={"sources"}
+                    
+      //             >
+      //               <h3 className="option">data sources</h3>
+      //             </li>
+      //             <li
+      //               className={"integrations"}
+      //               value={"integrations"}
+                    
+      //             >
+      //               <h3 className="option">Integrations</h3>
+      //             </li>
+      //             <li
+      //               className={"embedSite" }
+      //               value={"embedSite"}
+                   
+      //             >
+      //               <h3 className="option">Embed on site</h3>
+      //             </li>
+
+      //             <li
+      //               className={"history"}
+      //               value={"history"}
+                   
+      //             >
+      //               <h3 className="option">Conversation History</h3>
+      //             </li>
+
+      //             <li
+      //               className={"leads"}
+      //               value={"leads"}
+                    
+      //             >
+      //               <h3 className="option">Leads</h3>
+      //             </li>
+      //           </ul>
+      //           <hr />
+          
+      //     </div>
+      //   </div>
 
        
-      </div>
+      // </div>
     )
 }
 
