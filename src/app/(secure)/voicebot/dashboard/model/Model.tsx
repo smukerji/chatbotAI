cb44e85296bf151290eb26dd5e928ff880357b44
Rudresh-sisode <rudranrajput@gmail.com>
2024-09-12 18:04:20 +0530

    model value is storing in context | still working

--- src/app/(secure)/voicebot/dashboard/model/Model.tsx
+++ src/app/(secure)/voicebot/dashboard/model/Model.tsx
@@ -19,75 +19,74 @@ function Model() {
   const [stepsCount, setStepsCount] = useState<number>(5);
   const [firstMessage, setFirstMessage] = useState<string>("");
   const [systemPrompt, setSystemPrompt] = useState<string>("default");
+  const [selectedProvider, setSelectedProvider] = useState<string | undefined>(undefined);
+  const [models, setModels] = useState<{ value: string; label: string }[]>([]);
+  const [selectedModel, setSelectedModel] = useState<string | undefined>(undefined);
 
 
 
   useEffect(() => {
     // Set the initial value from the context
     setFirstMessage(voicebotDetails.firstMessage || "");
-    setSystemPrompt(voicebotDetails["model"]["messages"][0]["context"] || "");
-  }, [voicebotDetails.firstMessage, voicebotDetails["model"]["messages"][0]["context"]]);
+    setSystemPrompt(voicebotDetails["model"]["messages"][0]["content"] || "");
+    setSelectedProvider(voicebotDetails["model"]["provider"] || undefined);
+
+  }, [voicebotDetails.firstMessage, voicebotDetails["model"]["messages"][0]["content"], voicebotDetails["model"]["provider"]]);
 
   // voiceBotContextData.updateState("provider", "11labs");
 
   const firstMessageEnterHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
-    console.log("first message ", e.target.value);
     const enteredValue: string = e.target.value;
     setFirstMessage(enteredValue);
-    // voiceBotContextData.updateState("firstMessage", enteredValue);
     voiceBotContextData.updateTheVoiceBotInfo("firstMessage")(enteredValue);
-    console.log("first message from the context after updated ",voicebotDetails);
   }
 
   const systemPromptEnterHandler = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
-    console.log("system prompt ", e.target.value);
     const enteredValue: string = e.target.value;
     setSystemPrompt(enteredValue);
-    // voiceBotContextData.updateState("model.messages[0].context", enteredValue);
-    voiceBotContextData.updateTheVoiceBotInfo("model.messages[0].context")(enteredValue);
-    console.log("system prompt from the context after updated ",voicebotDetails);
+    voiceBotContextData.updateState("model.messages.0.content", enteredValue);
+  }
+
+  const providerChangeHandler = (value: string,option:any) => {
+    debugger;
+    setSelectedProvider(option.label);
+    voiceBotContextData.updateState("model.provider", option.label);
+    // console.log("Selected provider:", value," option ", option);
+
+    // Update models based on selected provider
+    const selectedProvider = providersOption.find(provider => provider.label === option.label);
+    if (selectedProvider) {
+      setModels(selectedProvider.models.map(model => ({ value: model, label: model })));
+    } else {
+      setModels([]);
+    }
   }
 
   console.log("your voicebot details ", voicebotDetails);
   
-  const providersOption: {value: string; label: string; }[] = [
-      {
-        value: '1',
-        label: 'openai',
-      },
-      {
-        value: '2',
-        label: 'together.ai',
-      },
-      {
-        value: '3',
-        label: 'anyscale',
-      },
-      {
-        value: '4',
-        label: 'openrouter',
-      },
-      {
-        value: '5',
-        label: 'perplexity-ai',
-      },
-      {
-        value: '6',
-        label: 'deepinfra',
-      },
-      {
-        value: '7',
-        label: 'groq',
-      },
-      {
-        value: '8',
-        label: 'anthropic',
-      },
-      {
-        value: '9',
-        label: 'custom-llm',
-      },
-    ]
+  const providersOption: {value: string; label: string; models:string[]}[] = [
+    {
+      value: '1',
+      label: 'openai',
+      models: [
+        "GPT 40 Cluster",
+        "GPT 40 Mini Cluster",
+        "GPT 3.5 Turbo Cluster",
+        "GPT 4 Turbo Cluster"
+      ]
+    },
+    {
+      value: '8',
+      label: 'anthropic',
+      models: [
+        "claude-3-opus-20240229",
+        "claude-3-sonnet-20240229",
+        "claude-3-haiku-20240307",
+        "claude-3-5-sonnet-20240620"
+      ]
+    }
+
+  ]
 
   return (
     <div className="model-container">
@@ -99,59 +98,39 @@ function Model() {
         placeholder="Hi, Provide me the first message!" 
         onChange={firstMessageEnterHandler}  
         value={firstMessage}/>
-
         <h4 className="input-header second">System Prompt</h4>
         <p className="input-description">The context allows you to customize your voicebot&lsquo;s personality, role and instructions. </p>
         <TextArea className="text-area"
          rows={4} 
          placeholder="Write your system prompts here..." 
-          value={systemPrompt}
-          onChange={systemPromptEnterHandler}
+         value={systemPrompt}
+         onChange={systemPromptEnterHandler}
          />
-
       </div>
 
       <div className="right-column">
         <h4 className="provider">Provider</h4>
         <Select
           className="select-field"
-
           placeholder="Select the provider"
-
+          onChange={providerChangeHandler}
+          value={selectedProvider}
           options={providersOption}
         />
 
         <h4 className="provider model">Model</h4>
         <Select
           className="select-field"
-
           placeholder="Select the model"
-
-
-          options={[
-            {
-              value: '1',
-              label: 'deepgram',
-            },
-            {
-              value: '2',
-              label: 'talkscriber',
-            },
-            {
-              value: '3',
-              label: 'gladia',
-            }
-          ]}
+          value={selectedModel}
+          options={models}
         />
-        <p className="model-info">GPT-4 is more accurate but slower and costlier than GPT-3.5 Turbo (1 min = 1 credit for GPT-3.5 Turbo, 20 credits for GPT-4).</p>
+        <p className="model-info">GPT-4o is more accurate but fast and cheaper</p>
 
         <h4 className="provider knowledge-base">Knowledge Base</h4>
         <Select
           className="select-field"
-
           placeholder="Select the model"
-
-
           options={[
             {
               value: '1',
