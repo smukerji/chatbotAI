33053cfacc22e06f3103deeb212d9dd9ad6a8c09
Rudresh-sisode <rudranrajput@gmail.com>
2025-01-20 13:08:52 +0530

    Enhance PhoneNumber component with data fetching and loading state management

--- src/app/(secure)/voicebot/dashboard/phone-number/PhoneNumber.tsx
+++ src/app/(secure)/voicebot/dashboard/phone-number/PhoneNumber.tsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import "./phone-number-design.scss";
 import PhoneInput from "react-phone-input-2";
-import { Input, Slider, Switch, Button, Select } from 'antd';
+import { Input, Slider, Switch, Button, Select ,  Flex, Spin} from 'antd';
+import Image from "next/image";
 import ImportNumber from './import-number/ImportNumber';
-import ImportNumberCustomModel from '../../../create-first-assistant/_components/ImportNumberCustomModel/ImportNumberCustomModel';
+import phoneNumberDelete from  "../../../../../../public/voiceBot/phone-number/trash.svg";
 
+import { useCookies } from "react-cookie";
+import ImportNumberCustomModel from '../../../create-first-assistant/_components/ImportNumberCustomModel/ImportNumberCustomModel';
+interface TwilioDetails {
+  createdAt: string;
+  id: string;
+  number: string;
+  orgId: string;
+  provider: string;
+  twilioAccountSid: string;
+  twilioAuthToken: string;
+  updatedAt: string;
+}
+
+interface InboundNumberDetails {
+  label: string;
+  twilio: TwilioDetails;
+  userId: string;
+  _id: string;
+}
 function PhoneNumber() {
   const contacts = [
     { id: 1, phone: '+84 (353) 085 867', email: 'tuan@sapakh.ai', toggle: true },
@@ -15,33 +35,148 @@ function PhoneNumber() {
     { id: 5, phone: '+84 (353) 085 867', email: 'tuan@sapakh.ai', toggle: false }
 
   ];
+  // let phoneNumber:any = [];
 
-  const [openModel, setOpenModel] = useState<boolean>(true);
+  const [phoneNumbers, setPhoneNumbers] = useState([]);
+  const [publishAssistantList, setPublishAssistantList] = useState([{ value: '', label: '' ,assistantId:''}]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [inboundNumberDetails, setInboundNumberDetails] = useState<InboundNumberDetails | null>(null);
+
+  const [cookies, setCookie] = useCookies(["userId"]);
+
+  const [openModel, setOpenModel] = useState<boolean>(false);
 
   const openHandleModel = () => {
     setOpenModel(true);
 
   }
 
-  return (
-    <div className='phone-container'>
-      <div className='left-container'>
-        <div className='list-items'>
-          {
+  async function getPublishAssistantDataFromDB() {
 
-            contacts.map((contact, index) => (
-              <>
-                {index !== 0 && <hr className="splitter" />}
-                <div className='list-item'>
-                  <div className='number-details'>
-                    <h2> {contact.phone} </h2>
-                    <p>  {contact.email} </p>
+    try {
+      const assistantListFromDb: any = await fetch(
+        `${process.env.NEXT_PUBLIC_WEBSITE_URL}voicebot/dashboard/api/phone/vapi-connect-assistant?userId=${cookies?.userId}`,
+        {
+          method: "GET",
+          next: { revalidate: 0 },
+        }
+      );
+      const assistantResult = await assistantListFromDb.json();
+      const assistantDataMap = assistantResult?.assistants.map((assistant: any) => {
+        return {
+          value: assistant.assistantName,
+          label: assistant.assistantName,
+          assistantId: assistant.vapiAssistantId
+        }
+      });
+      setPublishAssistantList(assistantDataMap);
+
+      console.log('assistant list:', assistantResult);
+    }
+    catch (error: any) {
+      console.error('Error parsing request body:', error);
+    }
+  }
+
+ async function getImportedTwilioDataFromDB() {
+
+  try{
+    setIsLoading(true);
+    
+  const phoneNumberData:any = await fetch(
+    `${process.env.NEXT_PUBLIC_WEBSITE_URL}voicebot/dashboard/api/phone?userId=${cookies?.userId}`,
+    {
+      method: "GET",
+      next: { revalidate: 0 },
+    }
+  );
+  const twilioNumbers = await phoneNumberData.json();
+  setPhoneNumbers(twilioNumbers?.importedNumbers);
+  debugger
+  setInboundNumberDetails(twilioNumbers?.importedNumbers[0]);
+  console.log('twilioNumbers:', twilioNumbers);
+  }
+  catch(error:any){
+    console.error('Error parsing request body:', error);
+  }
+  setIsLoading(false);
+
+  }
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      await getImportedTwilioDataFromDB();
+      await getPublishAssistantDataFromDB();
+    }
+    fetchData();
+
+  }, []);
+
+  function changedTheInboundNumberHandler(contact:any){
+    setInboundNumberDetails(contact);
+  }
+
+  function assistantSelectOnPhoneNumberHandler(option:any,values:any){
+    console.clear();
+    console.log('selected option:', option);
+    console.log('selected values:', values);
+  }
+
+  async function deletePhoneNumberHandler() {
+      console.clear();
+      console.log("Phone number details ", inboundNumberDetails);
+  
+      if (!inboundNumberDetails || !inboundNumberDetails._id) {
+          console.error("No phone number selected or missing ID");
+          return;
+      }
+  
+      try {
+          const response = await fetch(
+              `${process.env.NEXT_PUBLIC_WEBSITE_URL}voicebot/dashboard/api/phone?phoneNumberId=${inboundNumberDetails._id}`,
+              {
+                  method: "DELETE",
+                  next: { revalidate: 0 },
+              }
+          );
+  
+          const data = await response.json();
+          console.log('Delete response:', data.data);
+          await getImportedTwilioDataFromDB();
+          await getPublishAssistantDataFromDB();
+  
+      } catch (error: any) {
+          console.error('Error deleting phone number:', error);
+      }
+  }
+
+  return (
+    <>
+   {   isLoading ? (
+          <Flex align="center" gap="middle" className="loader">
+            <Spin size="large" />
+          </Flex>
+        ) :
+
+        <div className='phone-container'>
+        <div className='left-container'>
+          <div className='list-items'>
+            {
+            phoneNumbers.length > 0 ? 
+            phoneNumbers.map((contact:any, index:number) => (
+                <>
+                  {index !== 0 && <hr className="splitter" />}
+                  <div className='list-item' onClick={changedTheInboundNumberHandler.bind(null,contact)}>
+                    <div className='number-details'>
+                      <h2> {contact.twilio.number} </h2>
+                      <p>  {contact.label} </p>
+                    </div>
+                    <div className='switch-input'>
+                      <Switch className="switch-btn" defaultChecked />
+                    </div>
                   </div>
-                  <div className='switch-input'>
-                    <Switch className="switch-btn" defaultChecked />
-                  </div>
-                </div>
-              </>
+                </>
 
               ))
               :
@@ -78,108 +213,80 @@ function PhoneNumber() {
                   disabled />
                 </div>
 
-              <div className='input-wrapper'>
-                <h4 className="lable">Assistant</h4>
-                <Select className="select-field"
+                <div className='input-wrapper'>
+                  <h4 className="lable">Assistant</h4>
+                  <Select className="select-field"
 
-                  placeholder="Select the assistant"
+                    placeholder="Select the assistant"
 
+                    onSelect={assistantSelectOnPhoneNumberHandler}
 
-                  options={[
-                    {
-                      value: '1',
-                      label: 'deepgram',
-                    },
-                    {
-                      value: '2',
-                      label: 'talkscriber',
-                    },
-                    {
-                      value: '3',
-                      label: 'gladia',
-                    }
-                  ]}
-                />
-              </div>
-
-              <div className='select-wrapper'>
-                <h4 className='lable'>
-                  Fallback Destination
-                </h4>
-                <p className='description'>
-                  Set a fallback destination for inbound calls when the assistant or squad is not available.
-                </p>
-                <div className="phone-input-with-flag">
-                  <PhoneInput
-                    country={'us'}
+                    options={publishAssistantList}
                   />
                 </div>
+
+                <div className='select-wrapper'>
+                  <h4 className='lable'>
+                    Fallback Destination
+                  </h4>
+                  <p className='description'>
+                    Set a fallback destination for inbound calls when the assistant or squad is not available.
+                  </p>
+                  <div className="phone-input-with-flag">
+                    <PhoneInput
+                      country={'us'}
+                    />
+                  </div>
+                </div>
               </div>
-
-
             </div>
+
           </div>
 
-        </div>
+          <div className="bottom">
+            <div className="container">
+              <h3 className="title">Outbound Form</h3>
+              <p className="description">You can assign an outbound phone number , set up a fallback and set up a squad to be called if the assistant is not available.</p>
+              <div className="content-wrapper">
+              
+                <div className='select-wrapper'>
+                  <h4 className='lable'>
+                    Outbound Phone Number
+                  </h4>
+                
+                  <div className="phone-input-with-flag">
+                    <PhoneInput
+                      country={'us'}
+                      placeholder='Enter a phone number'
+                    />
+                  </div>
+                </div>
 
-        <div className="bottom">
-          <div className="container">
-            <h3 className="title">Outbound Form</h3>
-            <p className="description">You can assign an outbound phone number , set up a fallback and set up a squad to be called if the assistant is not available.</p>
-            <div className="content-wrapper">
-             
-              <div className='select-wrapper'>
-                <h4 className='lable'>
-                  Outbound Phone Number
-                </h4>
-               
-                <div className="phone-input-with-flag">
-                  <PhoneInput
-                    country={'us'}
-                    placeholder='Enter a phone number'
+                <div className='input-wrapper'>
+                  <h4 className="lable">Assistant</h4>
+                  <Select className="select-field"
+                    placeholder="Select the assistant"
+                    options={publishAssistantList}
                   />
                 </div>
+
+
               </div>
-
-              <div className='input-wrapper'>
-                <h4 className="lable">Assistant</h4>
-                <Select className="select-field"
-
-                  placeholder="Select the assistant"
-
-
-                  options={[
-                    {
-                      value: '1',
-                      label: 'deepgram',
-                    },
-                    {
-                      value: '2',
-                      label: 'talkscriber',
-                    },
-                    {
-                      value: '3',
-                      label: 'gladia',
-                    }
-                  ]}
-                />
-              </div>
-
-
             </div>
           </div>
         </div>
-      </div>
 
-      {
-        openModel &&
-        <ImportNumberCustomModel
-          setOpen={setOpenModel}
-          title={"Create a Tool"}
-          content={<ImportNumber />} />
-      }
+        {
+          openModel &&
+          <ImportNumberCustomModel
+            setOpen={setOpenModel}
+            title={"Create a Tool"}
+            content={<ImportNumber />} />
+        }
 
-    </div>
+      </div>}
+    </>
+ 
   )
 }
 
