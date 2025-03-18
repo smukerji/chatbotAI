de11c48963da55f80702dd0dc6204a8398883af2
Rudresh-Sisodiya <52784991+Rudresh-sisode@users.noreply.github.com>
2025-03-18 15:54:09 +0530

    feat: add tooltip for assistant info in ChooseVoiceAssistantExpert and ChooseVoiceAssistantType components

--- src/app/(secure)/create-first-assistant/_components/ChooseVoiceAssistantType/ChooseVoiceAssistantType.tsx
+++ src/app/(secure)/create-first-assistant/_components/ChooseVoiceAssistantType/ChooseVoiceAssistantType.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "next/image";
 
 import infoImage from "../../../../../../public/voiceBot/SVG/info-circle.svg";
+import { Tooltip } from 'antd';
+
 
 import customTemplate from "../../../../../../public/voiceBot/SVG/profile-circle.svg";
 
@@ -52,7 +54,9 @@ const ChooseVoiceAssistantType: React.FC<AssistantListProps> = ({
               <div className="header_container">
                 <h2 className="card_header">{assistant.assistantType}</h2>
                 <div className="image-info">
+                <Tooltip placement="bottomRight" title={assistant.info}>
                   <Image src={infoImage} alt="" height={100} width={100} />
+                </Tooltip>
                 </div>
               </div>
               <h3 className="card_sub-header">{assistant.dispcrtion}</h3>
