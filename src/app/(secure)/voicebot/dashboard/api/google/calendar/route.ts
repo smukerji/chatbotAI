a589f910ab458182dd5c5a1c5e7c0081f97225c9
Rudresh-Sisodiya <52784991+Rudresh-sisode@users.noreply.github.com>
2025-07-21 12:15:02 +0530

    fix: Update Google OAuth environment variables for consistency in calendar integration

--- src/app/(secure)/voicebot/dashboard/api/google/calendar/route.ts
+++ src/app/(secure)/voicebot/dashboard/api/google/calendar/route.ts
@@ -1,16 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { google } from "googleapis";
-
+/**
+ * 
+ * @param req        
+ *  process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID_TORI_VAPI_GOOGLE_CAL,
+ * process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET_TORI_VAPI_GOOGLE_CAL,
+ * process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI_TORI_VAPI_GOOGLE_CAL
+ * @returns 
+ */
 export async function GET(req: NextRequest) {
-    console.log({clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-        redirectUri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
+    console.log({clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID_TORI_VAPI_GOOGLE_CAL,
+        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET_TORI_VAPI_GOOGLE_CAL,
+        redirectUri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI_TORI_VAPI_GOOGLE_CAL,
         apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY
     });
+    
     const oauth2Client = new google.auth.OAuth2(
-        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-        process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI
+        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID_TORI_VAPI_GOOGLE_CAL,
+        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET_TORI_VAPI_GOOGLE_CAL,
+        process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI_TORI_VAPI_GOOGLE_CAL
     );
 
     const scopes = [
