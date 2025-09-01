d6b05a23389d20d83a59634f71481dbff33e7c47
Rudresh-Sisodiya <52784991+Rudresh-sisode@users.noreply.github.com>
2025-09-01 15:26:24 +0530

    feat: add new models for Google and update OpenAI models in the Model component

--- src/app/(secure)/voicebot/dashboard/model/Model.tsx
+++ src/app/(secure)/voicebot/dashboard/model/Model.tsx
@@ -234,7 +234,7 @@ function Model() {
       value: '1',
       label: 'openai',
       models: [
-        "gpt-4o-mini","gpt-4o-mini-2024-07-18", "gpt-4o", "gpt-4o-2024-05-13", "gpt-4o-2024-08-06", "gpt-4-turbo", "gpt-4-turbo-2024-04-09", "gpt-4-turbo-preview", "gpt-4-0125-preview", "gpt-4-1106-preview", "gpt-4", "gpt-4-0613", "gpt-3.5-turbo", "gpt-3.5-turbo-0125", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0613"
+        "gpt-5","gpt-5-nano","gpt-5-mini","gpt-4o-mini","gpt-4o-mini-2024-07-18", "gpt-4o", "gpt-4o-2024-05-13", "gpt-4o-2024-08-06", "gpt-4-turbo", "gpt-4-turbo-2024-04-09", "gpt-4-turbo-preview", "gpt-4-0125-preview", "gpt-4-1106-preview", "gpt-4", "gpt-4-0613", "gpt-3.5-turbo", "gpt-3.5-turbo-0125", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0613"
       ]
     },
     {
@@ -245,6 +245,13 @@ function Model() {
         "claude-3-sonnet-20240229",
         "claude-3-haiku-20240307",
         "claude-3-5-sonnet-20240620"
+      ]
+    },
+    {
+      value: '3',
+      label: 'google',
+      models: [
+        "gemini-2.5-pro", "gemini-2.5-flash", "gemini-2.5-flash-lite", "gemini-2.0-flash-thinking-exp", "gemini-2.0-pro-exp-02-05", "gemini-2.0-flash", "gemini-2.0-flash-lite", "gemini-2.0-flash-exp", "gemini-2.0-flash-realtime-exp", "gemini-1.5-flash", "gemini-1.5-flash-002", "gemini-1.5-pro", "gemini-1.5-pro-002", "gemini-1.0-pro",
       ]
     }
 
